460724ce2a24555e76efbd2a3bb25da20a3b990b
pradana <pradana.exe@gmail.com>
2012-07-24 12:35:36 +0700

    tambah file CI

---
+++ application/controllers/halo.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit ('No direct script');
+
+class Halo extends CI_Controller {
+	function index() 
+	{
+		$this->load->view("halo_view");
+	}
+}
+
+/* EOF halo.php */
+/* Location: ./application/controllers/halo.php */
---
+++ application/views/halo_view.php
@@ -0,0 +1,1 @@
+<h1>Hallo CodeIgniter</h1>
